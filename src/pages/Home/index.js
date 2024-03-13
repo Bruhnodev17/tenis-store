b4952eab82fc90286c15c595d5622ff28228b4c4
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-13 14:38:09 -0300

    ajustes de compatibilidade

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+ return (
+   <View>
+    <Text style={{marginTop: 60, fontSize:20, fontFamily: "Anton_400Regular"}}>Página Home</Text>
+   </View>
+  );
+}
