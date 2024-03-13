23a04999f80dff14940cc7497391c8fac74ca6e7
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-13 17:09:07 -0300

    carregamento de fonte

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import Routes from './src/router';
+import "react-native-gesture-handler";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import Routes from "./src/router";
 import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton";
 
 export default function App() {
-  
   let [fontsLoaded] = useFonts({
     Anton_400Regular,
   });
 
-  if (!fontsLoaded) { 
-    return ;
+  if (!fontsLoaded) {
   }
 
   return (
     <>
-      <StatusBar style="light" backgroundColor='#000' translucent={true} />
+      <StatusBar style="light" backgroundColor="#000" translucent={true} />
       <Routes />
     </>
   );
