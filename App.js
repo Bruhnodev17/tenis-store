32d3fb557bd881cee6c9f1ec5165324dd266ccc5
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-13 17:17:01 -0300

    upload assets

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import Routes from "./src/router";
-import { useFonts, Anton_400Regular } from "@expo-google-fonts/anton";
+import { useFonts, Anton_400Regular,} from "@expo-google-fonts/anton";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -10,6 +10,7 @@ export default function App() {
   });
 
   if (!fontsLoaded) {
+    return;
   }
 
   return (
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Home() {
  return (
    <View>
-    <Text style={{marginTop: 60, fontSize:20, fontFamily: "Anton_400Regular"}}>Página Home</Text>
+    <Text style={{marginTop: 60, fontSize:20, fontFamily: "Anton_400Regular", fontWeight: "bold"}}>Página Home</Text>
    </View>
   );
 }
