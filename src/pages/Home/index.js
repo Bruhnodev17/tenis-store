bedbe4e5c5df1dd5af5f94002d5b386f4cf6c4c1
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-15 12:54:28 -0300

    topo do app

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,65 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Home() {
- return (
-   <View>
-    <Text style={{marginTop: 60, fontSize:20, fontFamily: "Anton_400Regular", fontWeight: "bold"}}>Página Home</Text>
-   </View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../../assets/banner.png")}
+          style={styles.image}
+        />
+
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>TÊNIS</Text>
+          <Text style={[styles.text, { color: "#CECECF" }]}>●</Text>
+          <Text style={[styles.text, { color: "#CECECF" }]}>MASCULINO</Text>
+
+          <TouchableOpacity
+            style={{ position: "absolute", right: 0, alignSelf: "center" }}
+          >
+            <Ionicons name="list-sharp" size={24} color="#000" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.line}/>
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF",
+  },
+  header: {
+    marginBottom: 8,
+  },
+  image: {
+    width: "100%",
+  },
+  textContainer: {
+    flexDirection: "row",
+    marginVertical: "5%",
+    marginHorizontal: "5%",
+  },
+  text: {
+    fontFamily: "Anton_400Regular",
+    fontSize: 26,
+    marginHorizontal: " 1%",
+  },
+  line:{
+    borderBottomColor: "#d8d8d8",
+    borderBottomWidth: 2
+  }
+});
