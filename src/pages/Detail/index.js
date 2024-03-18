a330164b57430458311d03077822816fbb087fc4
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-18 10:32:30 -0300

    detalhes

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,36 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function Detail() {
- return (
-   <View>
-    <Text>Página Detalhe</Text>
-   </View>
+export default function Detail({ navigation }) {
+  navigation.setOptions({
+    headerTitle: "Nike Downshifter 10",
+  });
+
+  return (
+    <ScrollView style={styles.container}>
+      <Image
+        source={require("../../assets/detail.png")}
+        style={styles.image}
+        resizeMode="cover"
+      />
+
+      <View>
+        <View>
+          <Text style={[styles.title, { fontSize: 24 }]}>R$ 280</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {},
+  title: {},
+});
